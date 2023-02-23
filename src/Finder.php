ee22899c1966e6a12060e12fa42de6088368d6ef
Oleg Sedinkin <a@aw.wtf>
2023-02-23 20:33:21 +0600

    🚧

---
+++ src/Finder.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Hyqo\Finder;
+
+use Generator;
+use Hyqo\Finder\Exception\FinderException;
+use SplFileInfo;
+
+class Finder
+{
+    /**
+     * @param string $folder
+     * @return Generator<array-key,void,void,SplFileInfo>
+     */
+    protected function iterate(string $folder): Generator
+    {
+        if (!is_dir($folder)) {
+            return;
+        }
+
+        $folder = $this->normalizePath($folder);
+
+        $dir = new \RecursiveDirectoryIterator($folder, \RecursiveDirectoryIterator::SKIP_DOTS);
+        yield from new \RecursiveIteratorIterator($dir);
+    }
+
+    /**
+     * @param string $folder
+     * @param null|string $extension
+     * @return Generator<array-key,void,void,string>
+     */
+    public function find(string $folder, ?string $extension = null): Generator
+    {
+        foreach ($this->iterate($folder) as $file) {
+            if (!$file->isFile()) {
+                continue;
+            }
+
+            if (null === $extension || !strcasecmp($file->getExtension(), $extension)) {
+                yield $file->getPathname();
+            }
+        }
+    }
+
+    protected function normalizePath(string $path): string
+    {
+        return realpath($path);
+    }
+
+    public function load(string $filename): ?string
+    {
+        if (!file_exists($filename) || !is_file($filename)) {
+            return null;
+        }
+
+        return file_get_contents($filename);
+    }
+
+    public function touch(string $filename): bool
+    {
+        return $this->save($filename, '');
+    }
+
+    public function save(string $filename, string $content): bool
+    {
+        $this->mkdir(dirname($filename));
+
+        return false !== file_put_contents($filename, $content, LOCK_EX);
+    }
+
+    public function mkdir(string $folder, int $permissions = 0777): true
+    {
+        if (is_dir($folder)) {
+            return true;
+        }
+
+        if (!mkdir($folder, $permissions, true)) {
+            throw new FinderException(sprintf('Directory "%s" was not created', $folder));
+        }
+
+        return true;
+    }
+
+    public function symlink(string $target, string $link): bool
+    {
+        if (!is_file($target)) {
+            throw new FinderException(sprintf("This is not a file: %s", $target));
+        }
+
+        $folder = dirname($link);
+
+        $this->mkdir($folder);
+
+        if (is_file($link)) {
+            return true;
+        }
+
+        return symlink($target, $link);
+    }
+
+    public function isEmpty(string $folder): bool
+    {
+        return !glob("$folder/*");
+    }
+
+    public function flushFolder(string $folder): true
+    {
+        foreach (glob("$folder/*") as $file) {
+            if (is_dir($file)) {
+                $this->flushFolder($file);
+                rmdir($file);
+            } else {
+                unlink($file);
+            }
+        }
+
+        return true;
+    }
+
+    public function removeFolder(string $folder): true
+    {
+        $this->flushFolder($folder);
+
+        rmdir($folder);
+
+        return true;
+    }
+}
---
+++ tests/FinderTest.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Hyqo\Finder\Test;
+
+use Hyqo\Finder\Exception\FinderException;
+use Hyqo\Finder\Finder;
+use PHPUnit\Framework\TestCase;
+
+class FinderTest extends TestCase
+{
+    protected static string $folderA;
+    protected static string $folderB;
+    protected static string $folderBC;
+    protected static string $folderRO;
+    protected static string $folderC;
+    protected static string $folderD;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        $root = __DIR__ . '/var';
+        @mkdir($root, recursive: true);
+
+        self::$folderRO = "$root/ro";
+
+        self::$folderA = "$root/a";
+        self::$folderB = "$root/b";
+        self::$folderBC = "$root/b-copy";
+        self::$folderC = "$root/c";
+        self::$folderD = "$root/d";
+
+        @mkdir(self::$folderRO, 000, true);
+
+        self::fillFolder(self::$folderA);
+    }
+
+    protected static function fillFolder(string $folder): void
+    {
+        @mkdir($folder . '/sub/sub', recursive: true);
+
+        file_put_contents($folder . '/a.php', '');
+        file_put_contents($folder . '/b.php', '');
+        file_put_contents($folder . '/sub/c.php', '');
+        file_put_contents($folder . '/sub/sub/d.php', '');
+        file_put_contents($folder . '/e.txt', 'hello');
+        file_put_contents($folder . '/f', '');
+    }
+
+    public function test_find_php(): void
+    {
+        $finder = new Finder();
+
+        $files = iterator_to_array($finder->find(self::$folderA, 'PHP'));
+
+        $this->assertEquals([
+            self::$folderA . '/sub/sub/d.php',
+            self::$folderA . '/sub/c.php',
+            self::$folderA . '/b.php',
+            self::$folderA . '/a.php',
+        ], $files);
+    }
+
+    public function test_find_txt(): void
+    {
+        $finder = new Finder();
+
+        $files = iterator_to_array($finder->find(self::$folderA, 'Txt'));
+
+        $this->assertEquals([
+            self::$folderA . '/e.txt',
+        ], $files);
+    }
+
+    public function test_find_foo(): void
+    {
+        $finder = new Finder();
+
+        $files = iterator_to_array($finder->find(self::$folderA, 'foo'));
+
+        $this->assertEquals([], $files);
+    }
+
+    public function test_folder_does_not_exist(): void
+    {
+        $finder = new Finder();
+
+        $files = iterator_to_array($finder->find(self::$folderA . '/abc', 'php'));
+
+        $this->assertEquals([], $files);
+    }
+
+    public function test_successful_load(): void
+    {
+        $finder = new Finder();
+
+        $content = $finder->load(self::$folderA . '/e.txt');
+
+        $this->assertEquals("hello", $content);
+    }
+
+    public function test_failed_load(): void
+    {
+        $finder = new Finder();
+
+        $content = $finder->load(self::$folderA . '/abc.txt');
+
+        $this->assertNull($content);
+    }
+
+    public function test_successful_mkdir(): void
+    {
+        $finder = new Finder();
+
+        $folder = self::$folderA . '/foo';
+
+        try {
+            $result = $finder->mkdir($folder);
+
+            $this->assertTrue($result);
+        } finally {
+            rmdir($folder);
+        }
+    }
+
+    public function test_failed_mkdir(): void
+    {
+        $finder = new Finder();
+
+        $deniedFolder = self::$folderRO;
+
+        if (!is_dir($deniedFolder)) {
+            mkdir($deniedFolder, 0000);
+        }
+
+        $this->expectException(FinderException::class);
+        $finder->mkdir($deniedFolder . '/folder');
+    }
+
+    public function test_successful_symlink(): void
+    {
+        $finder = new Finder();
+
+        $targets = $finder->find(self::$folderA, 'php');
+
+        foreach ($targets as $target) {
+            $link = self::$folderC . '/' . basename($target);
+
+            $result = $finder->symlink($target, $link);
+
+            $this->assertTrue($result);
+            $this->assertTrue(is_link($link));
+            $this->assertEquals($target, readlink($link));
+        }
+    }
+
+    public function test_failed_symlink(): void
+    {
+        $finder = new Finder();
+
+        $target = __DIR__ . '/Fixtures/originals/abc';
+        $link = __DIR__ . '/Fixtures/links/abc';
+
+        $this->expectException(FinderException::class);
+        $finder->symlink($target, $link);
+    }
+
+    public function test_flush_folder(): void
+    {
+        $finder = new Finder();
+
+        self::fillFolder(self::$folderB);
+
+        foreach ($finder->find(self::$folderB) as $target) {
+            $finder->symlink($target, str_replace(self::$folderB, self::$folderBC, $target));
+        }
+
+        $this->assertFalse($finder->isEmpty(self::$folderBC));
+
+        $finder->flushFolder(self::$folderBC);
+
+        $this->assertTrue($finder->isEmpty(self::$folderBC));
+        $this->assertDirectoryExists(self::$folderBC);
+    }
+
+    public function test_remove_folder(): void
+    {
+        $finder = new Finder();
+
+        self::fillFolder(self::$folderB);
+
+        foreach ($finder->find(self::$folderB) as $target) {
+            $finder->symlink($target, str_replace(self::$folderB, self::$folderBC, $target));
+        }
+
+        $this->assertFalse($finder->isEmpty(self::$folderBC));
+
+        $finder->removeFolder(self::$folderBC);
+
+        $this->assertDirectoryDoesNotExist(self::$folderBC);
+    }
+}
